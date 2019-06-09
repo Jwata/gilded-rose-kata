8ca59b5962ba74c00c8c5f7b41be4e2fcd7ad8ea
Jwata <watanabe0621@gmail.com>
2019-06-10 07:49:15 +0900

    extract existing update logic

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -7,69 +7,72 @@ type Item struct {
 	sellIn, quality int
 }
 
-var items = []Item{
-	Item{"+5 Dexterity Vest", 10, 20},
-	Item{"Aged Brie", 2, 0},
-	Item{"Elixir of the Mongoose", 5, 7},
-	Item{"Sulfuras, Hand of Ragnaros", 0, 80},
-	Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
-	Item{"Conjured Mana Cake", 3, 6},
-}
-
-func main() {
-	fmt.Println("OMGHAI!")
-	// fmt.Print(items)
-	GildedRose()
-}
-
-func GildedRose() {
-	for i := 0; i < len(items); i++ {
-
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
+func (item *Item) update() {
+	if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
+		if item.quality > 0 {
+			if item.name != "Sulfuras, Hand of Ragnaros" {
+				item.quality = item.quality - 1
 			}
 		}
-
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
+	} else {
+		if item.quality < 50 {
+			item.quality = item.quality + 1
+			if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+				if item.sellIn < 11 {
+					if item.quality < 50 {
+						item.quality = item.quality + 1
 					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
 				}
-			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
+				if item.sellIn < 6 {
+					if item.quality < 50 {
+						item.quality = item.quality + 1
+					}
 				}
 			}
 		}
 	}
 
+	if item.name != "Sulfuras, Hand of Ragnaros" {
+		item.sellIn = item.sellIn - 1
+	}
+
+	if item.sellIn < 0 {
+		if item.name != "Aged Brie" {
+			if item.name != "Backstage passes to a TAFKAL80ETC concert" {
+				if item.quality > 0 {
+					if item.name != "Sulfuras, Hand of Ragnaros" {
+						item.quality = item.quality - 1
+					}
+				}
+			} else {
+				item.quality = item.quality - item.quality
+			}
+		} else {
+			if item.quality < 50 {
+				item.quality = item.quality + 1
+			}
+		}
+	}
+}
+
+var items = []*Item{
+	{"+5 Dexterity Vest", 10, 20},
+	{"Aged Brie", 2, 0},
+	{"Elixir of the Mongoose", 5, 7},
+	{"Sulfuras, Hand of Ragnaros", 0, 80},
+	{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+	{"Conjured Mana Cake", 3, 6},
+}
+
+func main() {
+	fmt.Println("OMGHAI!")
+	// fmt.Print(items)
+	GildedRose(items)
+}
+
+func GildedRose(items []*Item) {
+	for i := 0; i < len(items); i++ {
+		item := items[i]
+		item.update()
+	}
 }
--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -1,7 +1,20 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_GildedRose(t *testing.T) {
-	main()
+	item1 := &Item{"Test Item 1", 1, 1}
+	item2 := &Item{"Test Item 2", 2, 2}
+	testItems := []*Item{item1, item2}
+
+	GildedRose(testItems)
+
+	assert.Equal(t, testItems[0].sellIn, 0)
+	assert.Equal(t, testItems[0].quality, 0)
+	assert.Equal(t, testItems[1].sellIn, 1)
+	assert.Equal(t, testItems[1].quality, 1)
 }
